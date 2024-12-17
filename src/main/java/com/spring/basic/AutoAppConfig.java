d0adfdc1feed089aaa19cb0044a35d9beadc8fb7
heedae93 <hdh89900@gmail.com>
2024-12-17 21:32:19 +0900

    @ComponentScan 으로 자동으로 빈으로 등록을 하고 @Autowired로  자동 의존관계 주입

---
+++ src/main/java/com/spring/basic/AutoAppConfig.java
@@ -0,0 +1,17 @@
+package com.spring.basic;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+
+
+// @ComponentScan을 사용하면 @Component가 붙은 모든 클래스를  자동으로 스프링 빈으로 등록한다.
+// 아래 filter설정은 기존의 코드를 유지하기 위해서 @Configuration이 붙은 클래스는 제외하고 컴포넌트 스캔을 하겠다는 의미이다.
+// 실무적으로는 이렇게 하지 않아도 된다.
+@Configuration
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+public class AutoAppConfig {
+
+}
--- src/main/java/com/spring/basic/member/MemoryMemberRepository.java
+++ src/main/java/com/spring/basic/member/MemoryMemberRepository.java
@@ -1,9 +1,13 @@
 package com.spring.basic.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+
+@Component
 public class MemoryMemberRepository implements MemberRepository {
 
 
